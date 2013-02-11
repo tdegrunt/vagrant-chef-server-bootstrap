558cfb9ae1663015d9ffaf213212784b1d3ea87e
grosser <grosser.michael@gmail.com>
2013-02-11 07:31:27 -0800

    simplify fix

--- cookbooks/fixup/recipes/default.rb
+++ cookbooks/fixup/recipes/default.rb
@@ -1,13 +1,13 @@
 # There seems to be a problem with the installation, which seems related to the gem.
-# The chef deamons files in `/etc/init.d` have incorrect paths in their `DAEMON` parameter, incorrectly referring to `/usr/bin` instead of `/usr/sbin`. In the meantime this will solve the issue:
+# The chef daemons files in `/etc/init.d` have incorrect paths in their `DAEMON` parameter, incorrectly referring to `/usr/bin` instead of `/usr/sbin`. In the meantime this will solve the issue:
 execute "fix bin folders" do
   command <<-BASH.gsub("    ", "")
     # fix symlinks
     cd /usr/bin
-    ln -s /usr/sbin/chef-server-webui chef-server-webui
-    ln -s /usr/sbin/chef-server chef-server
-    ln -s /usr/sbin/chef-server chef-solr
-    ln -s /usr/sbin/chef-expander chef-expander
+    ln -s /usr/sbin/chef-server-webui
+    ln -s /usr/sbin/chef-server
+    ln -s /usr/sbin/chef-server
+    ln -s /usr/sbin/chef-expander
 
     # restart
     /etc/init.d/chef-server start
@@ -16,4 +16,5 @@ execute "fix bin folders" do
     /etc/init.d/chef-expander start
   BASH
   action :run
+  not_if "test -f /usr/bin/chef-expander"
 end
